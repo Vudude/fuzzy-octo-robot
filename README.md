cd6597f7ed4b3356a4ef6ab7cffe35792cdaf5b7
Vu Le <le@apparray.biz>
2019-03-24 17:01:41 +0900

    Add documentation; update git ignore; remove .gradlew

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fuzzy-octo-robot
-API to count number of a certain word for a given webpage
+Web service for counting the number of occurrences of a word in a single given URL html source.
+
+This application is deployed using Heroku at https://word-count-api.herokuapp.com/ and 
+can be used by sending a POST request.
+
+Documentation can be found at https://word-count-api.herokuapp.com/swagger-ui.html 
+
+
--- src/main/kotlin/com/example/demo/WordCountController.kt
+++ src/main/kotlin/com/example/demo/WordCountController.kt
@@ -1,5 +1,6 @@
 package com.example.demo
 
+import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,6 +12,11 @@ class WordCountController(
         private val wordCountService: WordCountService
 ) {
 
+    @ApiOperation(
+            value = "Counts appearances of a specified word on a given html source page",
+            notes = "Uses a Regex expression to find occurrences of given word. Does not find occurrences " +
+                    "that are inside of another word."
+    )
     @RequestMapping("/", method = [RequestMethod.POST])
     fun wordCount(@RequestBody request: WordCountRequest): WordCountResponse  {
         return wordCountService.countWords(request)
